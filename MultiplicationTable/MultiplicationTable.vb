59d83b08e4d26b652207425914d9679c3850a441
Chris Pickens <Cpickens1270@gmail.com>
2023-01-29 03:27:15 -0700

    Add project files.

---
+++ MultiplicationTable/MultiplicationTable.vb
@@ -0,0 +1,17 @@
+'Christopher Pickens
+'RCET0265
+'Spring 2023
+'Multiplication Table
+'https://github.com/Pickchr2/MultiplicationTable.git
+
+Option Explicit On
+Option Strict On
+Option Compare Text
+
+Imports System
+
+Module MultiplicationTable
+    Sub Main(args As String())
+
+    End Sub
+End Module
